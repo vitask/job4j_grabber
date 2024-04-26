cf4076c8ec40d09ff671769d46ef426719031b73
vitask <skalenko.v@yandex.ru>
2024-04-26 23:06:34 +0700

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -0,0 +1,34 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class HabrCareerParse {
+
+    private static final String SOURCE_LINK = "https://career.habr.com";
+    private static final String PREFIX = "/vacancies?page=";
+    private static final String SUFFIX = "&q=Java%20developer&type=all";
+
+    public static void main(String[] args) throws IOException {
+        int pageNumber = 1;
+        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+        Connection connection = Jsoup.connect(fullLink);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element vacancyDate = row.select(".vacancy-card__date").first();
+            Element linkElement = titleElement.child(0);
+            Element linkDate = vacancyDate.child(0);
+            String vacancyName = titleElement.text();
+            String date = linkDate.attr("datetime");
+            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            System.out.printf("%s %s %s%n", vacancyName, date, link);
+        });
+    }
+}
